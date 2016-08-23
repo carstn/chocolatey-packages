a001e1b63c1c84b313c27645fcc6d3b04e76528d
Carsten Ganske <carstenganske@gmail.com>
2016-08-23 01:54:34 -0700

    [golang] prepare for 1.6.3

--- golang/tools/chocolateyinstall.ps1
+++ golang/tools/chocolateyinstall.ps1
@@ -1,14 +1,6 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+﻿$ErrorActionPreference = 'Stop';
 
-$ErrorActionPreference = 'Stop'; # stop on all errors
-
-## Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
-## - https://chocolatey.org/docs/helpers-start-chocolatey-process-as-admin
-#Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes $validExitCodes
-
-$packageName = 'golang' # arbitrary name for the package, used in messages
+$packageName = 'golang'
 $url = 'https://storage.googleapis.com/golang/go1.6.3.windows-386.zip'
 $checksum = '3aa8c3208272143c2eadb67976e6e41048a95ff5ac0b55ea4b3b0c88a9ca1a8a'
 $url64 = 'https://storage.googleapis.com/golang/go1.6.3.windows-amd64.zip'
@@ -24,7 +16,5 @@ if (Test-Path "$goroot") {
 
 Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$tools" -Url64bit "$url64" -Checksum "$checksum" -ChecksumType 'sha256' -Checksum64 "$checksum64" -ChecksumType64 'sha256'
 
-## Sets the GOROOT environment variable. Adds binaries to the PATH.
-## Note: These 'Machine' variables will assert administrative rights
 Install-ChocolateyEnvironmentVariable -VariableName 'GOROOT' -VariableValue "$goroot" -VariableType 'Machine'
 Install-ChocolateyPath -PathToInstall "$gorootBin" -PathType 'Machine'
