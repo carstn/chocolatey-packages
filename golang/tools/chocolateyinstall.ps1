2ae321c2d203a937dfbbd61cec495a928d958edb
Carsten Ganske <carstenganske@gmail.com>
2016-08-23 01:25:33 -0700

    [golang] first pass at Go 1.6.3

---
+++ golang/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+## Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
+## - https://chocolatey.org/docs/helpers-start-chocolatey-process-as-admin
+#Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes $validExitCodes
+
+$packageName = 'golang' # arbitrary name for the package, used in messages
+$url = 'https://storage.googleapis.com/golang/go1.6.3.windows-386.zip'
+$checksum = '3aa8c3208272143c2eadb67976e6e41048a95ff5ac0b55ea4b3b0c88a9ca1a8a'
+$url64 = 'https://storage.googleapis.com/golang/go1.6.3.windows-amd64.zip'
+$checksum64 = '6a18e5ed8b39785338986aecc6a3f36f5c4be286ff52db0ae3bcd2275ab70df0'
+
+Get-ToolsLocation
+$tools = $env:ChocolateyToolsLocation
+$goroot = Join-Path "$tools" 'go'
+$gorootBin = Join-Path "$goroot" 'bin'
+if (Test-Path "$goroot") {
+  Remove-Item "$goroot" -Recurse
+}
+
+Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$tools" -Url64bit "$url64" -Checksum "$checksum" -ChecksumType 'sha256' -Checksum64 "$checksum64" -ChecksumType64 'sha256'
+
+## Sets the GOROOT environment variable. Adds binaries to the PATH.
+## Note: These 'Machine' variables will assert administrative rights
+Install-ChocolateyEnvironmentVariable -VariableName 'GOROOT' -VariableValue "$goroot" -VariableType 'Machine'
+Install-ChocolateyPath -PathToInstall "$gorootBin" -PathType 'Machine'
---
+++ golang/tools/chocolateyuninstall.ps1
@@ -0,0 +1,13 @@
+﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+
+## If this is an MSI, cleaning up comments is all you need.
+## If this is an exe, change installerType and silentArgs
+## Auto Uninstaller should be able to detect and handle registry uninstalls (if it is turned on, it is in preview for 0.9.9).
+## - https://chocolatey.org/docs/helpers-uninstall-chocolatey-package
+
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+#Uninstall-ChocolateyZipPackage "$packageName" # Only necessary if you did not unpack to package directory - see https://chocolatey.org/docs/helpers-uninstall-chocolatey-zip-package
+Uninstall-ChocolateyEnvironmentVariable -VariableName 'GOROOT' -VariableType 'Machine'
