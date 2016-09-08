24a890451e4fc425bd7eec76fd6acb52f1469d9e
Carsten Ganske <carstenganske@gmail.com>
2016-09-07 19:49:45 -0700

    Go1.7 (#3)
    * [golang] 1.7
    
    * [golang] remove leftover nupkg from 1.6.3

--- golang/tools/chocolateyinstall.ps1
+++ golang/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'golang'
-$url = 'https://storage.googleapis.com/golang/go1.6.3.windows-386.zip'
-$checksum = '3aa8c3208272143c2eadb67976e6e41048a95ff5ac0b55ea4b3b0c88a9ca1a8a'
-$url64 = 'https://storage.googleapis.com/golang/go1.6.3.windows-amd64.zip'
-$checksum64 = '6a18e5ed8b39785338986aecc6a3f36f5c4be286ff52db0ae3bcd2275ab70df0'
+$url = 'https://storage.googleapis.com/golang/go1.7.windows-386.zip'
+$checksum = '9a4323fde431f1638ac40a504c1a96f584b6a7a53931599f95df4c8dd530b627'
+$url64 = 'https://storage.googleapis.com/golang/go1.7.windows-amd64.zip'
+$checksum64 = 'f51aad06644cc8bd119d2f6933334fa8da24d26e6676fde022cecf5978f1a0c7'
 
 Get-ToolsLocation
 $tools = $env:ChocolateyToolsLocation
