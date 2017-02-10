351e2cd57bebdfc3adf9a728fff0cf0982e67917
Carsten Ganske <carstenganske@gmail.com>
2017-02-09 18:12:13 -0700

    Go1.6.4 (#10)
    * Remove old nupkg
    
    * update nuspec and install script for go1.6.4
    
    * [golang] add built nupkg for Go 1.6.4

--- golang/tools/chocolateyinstall.ps1
+++ golang/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'golang'
-$url = 'https://storage.googleapis.com/golang/go1.7.3.windows-386.zip'
-$checksum = 'd0ac2d3aaa20452d0f09112f034cca1c5b8560452a45e10523af7f0a1089c792'
-$url64 = 'https://storage.googleapis.com/golang/go1.7.3.windows-amd64.zip'
-$checksum64 = '9fe41313b97e2a6a703f5ae22938c7d9ac4336a128b522376c224ba97e8c7f01'
+$url = 'https://storage.googleapis.com/golang/go1.6.4.windows-386.zip'
+$checksum = '91fb3e13dfba94c13334a3ce8362ed6dfd0e5e94c4a1251517223571c2410d73'
+$url64 = 'https://storage.googleapis.com/golang/go1.6.4.windows-amd64.zip'
+$checksum64 = '4fc871ac03f5ca5978ad2ae860192e6a39dc2b1286afbe86f4947faab84ab231'
 
 Get-ToolsLocation
 $tools = $env:ChocolateyToolsLocation
