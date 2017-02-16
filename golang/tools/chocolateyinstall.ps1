6f3be74030d280dd5692eb290abc7786d4dcd36f
Carsten Ganske <carstenganske@gmail.com>
2017-02-16 13:23:13 -0800

    Go 1.8 (#13)
    * [golang] Update Go to 1.8
    
    * [golang] Add .nupkg for Go 1.8

--- golang/tools/chocolateyinstall.ps1
+++ golang/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'golang'
-$url = 'https://storage.googleapis.com/golang/go1.7.5.windows-386.zip'
-$checksum = 'de367304d1d654ff23d74a2644a457b7740c2da46bc3abedc46f1317f97316ad'
-$url64 = 'https://storage.googleapis.com/golang/go1.7.5.windows-amd64.zip'
-$checksum64 = '01eb518cb5a12edd6cf7380ec17ebedee755e3ce7e5362febeebb9e70e45fcaa'
+$url = 'https://storage.googleapis.com/golang/go1.8.windows-386.zip'
+$checksum = 'b8199a4af9327807803d2892268dfbffb615f98d717cd824833a97e77f981f46'
+$url64 = 'https://storage.googleapis.com/golang/go1.8.windows-amd64.zip'
+$checksum64 = 'cb27fe210f3a9d10329d48514895d2a1e3651125a7c3c758f0358a5bfc0e3060'
 
 Get-ToolsLocation
 $tools = $env:ChocolateyToolsLocation
