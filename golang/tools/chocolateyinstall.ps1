a452c742564d33a73657150915364d994a0ccced
Carsten Ganske <carstenganske@gmail.com>
2017-04-07 13:21:37 -0700

    [golang] Update to Go 1.8.1 (#14)

--- golang/tools/chocolateyinstall.ps1
+++ golang/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'golang'
-$url = 'https://storage.googleapis.com/golang/go1.8.windows-386.zip'
-$checksum = 'b8199a4af9327807803d2892268dfbffb615f98d717cd824833a97e77f981f46'
-$url64 = 'https://storage.googleapis.com/golang/go1.8.windows-amd64.zip'
-$checksum64 = 'cb27fe210f3a9d10329d48514895d2a1e3651125a7c3c758f0358a5bfc0e3060'
+$url = 'https://storage.googleapis.com/golang/go1.8.1.windows-386.zip'
+$checksum = '9738365e64d4d80ea54487915c9bfc0c3bbaa7d68ec78487988f80ebe8129c57'
+$url64 = 'https://storage.googleapis.com/golang/go1.8.1.windows-amd64.zip'
+$checksum64 = 'bb6f0fbef8b80c382455af8699bfbb7fe89256d4baf06d927feaeceb7342e4ee'
 
 Get-ToolsLocation
 $tools = $env:ChocolateyToolsLocation
