c769438a037de07fe431ec3fada30db0912eee6f
Carsten Ganske <carstenganske@gmail.com>
2018-08-29 20:58:05 -0700

    [golang] update Go to 1.10.4

--- golang/tools/chocolateyinstall.ps1
+++ golang/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'golang'
-$url = 'https://dl.google.com/go/go1.10.3.windows-386.zip'
-$checksum = '89696a29bdf808fa9861216a21824ae8eb2e750a54b1424ce7f2a177e5cd1466'
-$url64 = 'https://dl.google.com/go/go1.10.3.windows-amd64.zip'
-$checksum64 = 'a3f19d4fc0f4b45836b349503e347e64e31ab830dedac2fc9c390836d4418edb'
+$url = 'https://dl.google.com/go/go1.10.4.windows-386.zip'
+$checksum = '407e5619048c427de4a65b26edb17d54c220f8c30ebd358961b1785a38394ec9'
+$url64 = 'https://dl.google.com/go/go1.10.4.windows-amd64.zip'
+$checksum64 = '5499aa98399664df8dc1da5c3aaaed14b3130b79c713b5677a0ee9e93854476c'
 
 Get-ToolsLocation
 $tools = $env:ChocolateyToolsLocation
